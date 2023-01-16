59ec4a867169d336ce73dbc5e5b9be37531aad54
Gil Teitelbaum <gt435q@att.com>
2023-01-16 09:34:48 +0200

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+/*
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+}
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+}
+*/
+test('renders learn react link', () => {
+     render(<App />);
+  const linkElement = screen.getByText(/Another Change/i);
+  expect(linkElement).toBeInTheDocument();
+});
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const linkElement = screen.getByText(/Another Change/i);
+expect(linkElement).toBeInTheDocument();
 });
